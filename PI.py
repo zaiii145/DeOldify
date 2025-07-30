51d828ef9268ccdc6fbf4a4526a345ae3cfdb584
Zaina <zainafathimai24@gmail.com>
2025-07-31 01:21:06 +0530

    final commit

--- PI.py
+++ PI.py
@@ -1,60 +1,65 @@
 import numpy as np
-import argparse
 import cv2
+import streamlit as st
 import os
+from io import BytesIO
+from PIL import Image
 
-DIR=r"C:\Users\zaina\OneDrive\Desktop\colour"
-PROTOTXT=os.path.join(DIR,r"COLOURIZATION\colorization_deploy_v2.prototxt")
-POINTS=os.path.join(DIR,r"COLOURIZATION\pts_in_hull.npy")
-MODEL=os.path.join(DIR,r"COLOURIZATION\colorization_release_v2.caffemodel")
+PROTOTXT = "For Colorisation/colorization_deploy_v2.prototxt"
+POINTS = "For Colorisation/pts_in_hull.npy"
+MODEL = "For Colorisation/colorization_release_v2.caffemodel"
 
-ap=argparse.ArgumentParser()
-ap.add_argument("-i","--image",type=str, required=True,
-                help="path to input black and white image")
-args=vars(ap.parse_args())
+net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+pts = np.load(POINTS)
+class8 = net.getLayerId("class8_ab")
+conv8 = net.getLayerId("conv8_313_rh")
+pts = pts.transpose().reshape(2, 313, 1, 1)
+net.getLayer(class8).blobs = [pts.astype("float32")]
+net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
 
-print("Load model")
-net=cv2.dnn.readNetFromCaffe(PROTOTXT,MODEL)
-pts=np.load(POINTS)
+def colorize_image(image):
+    scaled = image.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+    
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+    
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
 
-class8=net.getLayerId("class8_ab")
-conv8=net.getLayerId("conv8_313_rh")
-pts=pts.transpose().reshape(2,313,1,1)
-net.getLayer(class8).blobs=[pts.astype("float32")]
-net.getLayer(conv8).blobs=[np.full([1,313],2.606,dtype="float32")]
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+    colorized = (255 * colorized).astype("uint8")
+    
+    return colorized
 
-image=cv2.imread(args["image"])
-scaled=image.astype("float32")/255.0
-lab=cv2.cvtColor(scaled,cv2.COLOR_BGR2LAB)
 
-resized=cv2.resize(lab,(224,224))
-L=cv2.split(resized)[0]
-L-=50
+st.title("DeOldify")
 
-print("Colorizing the image")
-net.setInput(cv2.dnn.blobFromImage(L))
-ab=net.forward()[0,:,:,:].transpose((1,2,0))
+uploaded_file = st.file_uploader("Upload a black-and-white image", type=["jpg", "jpeg", "png","webp"])
 
-ab=cv2.resize(ab,(image.shape[1],image.shape[0]))
+if uploaded_file is not None:
 
-L=cv2.split(lab)[0]
-colorized=np.concatenate((L[:,:,np.newaxis],ab),axis=2)
+    image = Image.open(uploaded_file)
+    image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-colorized=cv2.cvtColor(colorized,cv2.COLOR_LAB2BGR)
-colorized=np.clip(colorized,0,1)
+    colorized_cv = colorize_image(image_cv)
+    colorized = Image.fromarray(cv2.cvtColor(colorized_cv, cv2.COLOR_BGR2RGB))
 
-colorized=(255* colorized).astype("uint8")
+    col1, col2 = st.columns(2)
+    col1.image(image, caption="Original Image", use_container_width=True)
+    col2.image(colorized, caption="Colorised Image", use_container_width=True)
 
-cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Colorized", cv2.WINDOW_NORMAL)
-
-# Set the window sizes to desired dimensions (e.g., 600x400)
-cv2.resizeWindow("Original", 800, 400)
-cv2.resizeWindow("Colorized", 800, 400)
-
-cv2.imshow("Original", image)
-cv2.imshow("Colorized", colorized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#Switch to the folder where this project is via command prompt
-#Run this Command   python p1.py --image images/space4.jpg
+    buf = BytesIO()
+    colorized.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    st.download_button(
+        label="Download Colorised Image",
+        data=byte_im,
+        file_name="colorised.png",
+        mime="image/png"
+    )
